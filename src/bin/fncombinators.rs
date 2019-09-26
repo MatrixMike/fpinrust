b84e79da5cdef7f22ed1fef31aa623bc493478b1
Mike H <matroxmike@icloud.com>
2019-09-26 14:37:52 +1000

    experiments 2

--- src/bin/fncombinators.rs
+++ src/bin/fncombinators.rs
@@ -1,39 +1,43 @@
 //function combinators example
 fn main() {
- let vector = vec!(1,3,4,5,3);
-  
- //map values
- let mapped_vector = vector.iter().map(|&x| x +1).collect::<Vec<i32>>();
- println!("{:?}",mapped_vector);
+    let vector = vec![1, 3, 4, 5, 3];
 
- //filter values
- let filtered_values = vector.iter().filter(|&x| x%2 ==0).collect::<Vec<&i32>>();
- println!("{:?}",filtered_values);
+    //map values
+    let mapped_vector = vector.iter().map(|&x| x + 1).collect::<Vec<i32>>();
+    println!("{:?}", mapped_vector);
 
- //count number of items
- let vec_count = vector.iter().count();
- println!("vector count  is {}",vec_count);
+    //filter values
+    let filtered_values = vector.iter().filter(|&x| x % 2 == 0).collect::<Vec<&i32>>();
+    println!("{:?}", filtered_values);
 
- //zip with index
- let index_vec = 0..vec_count;
- let index_zipped_vector = vector.iter().zip(index_vec).collect::<Vec<(&i32,usize)>>(); 
- println!("zipped vector is {:?}",index_zipped_vector);
+    //count number of items
+    let vec_count = vector.iter().count();
+    println!("vector count  is {}", vec_count);
 
- //fold to find the sum
- 
- let sum = vector.iter().fold(0,(|sum,value| sum+value));
- println!("sum of the vector is {}",sum);
+    //zip with index
+    let index_vec = 0..vec_count;
+    let index_zipped_vector = vector.iter().zip(index_vec).collect::<Vec<(&i32, usize)>>();
+    println!("zipped vector is {:?}", index_zipped_vector);
 
- //find max 
- let max = vector.iter().max().unwrap();
- println!("max is {}",max);
+    //fold to find the sum
 
- //forall 
- let greater_than_zero = vector.iter().all(|&x| x > 0 ) ;
- println!("greater than zero {}",greater_than_zero);
+    let sum = vector.iter().fold(0, |sum, value| sum + value);
+    println!("sum of the vector is {}", sum);
 
- //list of words using flat map
- let lines_vec = vec!("hello,how","are,you");
- let words_vec = lines_vec.iter().flat_map(|&x| x.split(",")).collect::<Vec<&str>>();
- println!("{:?}", words_vec);
+    //find max
+    let max = vector.iter().max().unwrap();
+    println!("max is {}", max);
+
+    //forall
+    let greater_than_zero = vector.iter().all(|&x| x > 0);
+    println!("greater than zero {}", greater_than_zero);
+
+    //list of words using flat map
+    let lines_vec = vec!["hello,how", "are,you"];
+    let words_vec = lines_vec
+        .iter()
+        .flat_map(|&x| x.split(","))
+        .collect::<Vec<&str>>();
+    println!("{:?}", words_vec);
 }
+
